adb94e43e7c1a119cc21c83c23b05d57709c9d36
Sounghan Kim <sounghan.kim@allganize.ai>
2024-06-25 23:46:30 +0900

    Ch03-03 Char

---
+++ src/ch03/Ch03_03_String.java
@@ -0,0 +1,14 @@
+package ch03;
+
+public class Ch03_03_String {
+    public static void main(String[] args) {
+        char a1 = 'a'; // 문자로 표현 (홑따옴표 사용해야 함)
+        char a2 = 97;  // 아스키코드로 표현
+        char a3 = '\u0061'; // 유니코드로 표현
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+
+    }
+}
